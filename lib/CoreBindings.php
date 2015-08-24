deffd2208dcdd33b5e875d1e5c3cba11f4a8bbe6
Olivier Laviale <olivier.laviale@gmail.com>
2015-08-24 20:36:37 +0200

    Tidying

--- lib/CoreBindings.php
+++ lib/CoreBindings.php
@@ -18,6 +18,8 @@ use ICanBoogie\Render\TemplateResolver;
 /**
  * {@link \ICanBoogie\Core} prototype bindings.
  *
+ * @method string render($target_or_options, array $additional_options = [])
+ *
  * @property EngineCollection $template_engines
  * @property TemplateResolver $template_resolver
  * @property Renderer $renderer
@@ -25,21 +27,9 @@ use ICanBoogie\Render\TemplateResolver;
  * @see \ICanBoogie\Binding\Render\Hooks::get_template_engines
  * @see \ICanBoogie\Binding\Render\Hooks::get_template_resolver
  * @see \ICanBoogie\Binding\Render\Hooks::get_renderer
+ * @see \ICanBoogie\Binding\Render\Hooks::render
  */
 trait CoreBindings
 {
-	/**
-	 * Renders a template.
-	 *
-	 * @see \ICanBoogie\Binding\Render\Hooks::render
-	 *
-	 * @param mixed $target_or_options
-	 * @param array $additional_options
-	 *
-	 * @return mixed
-	 */
-	public function render($target_or_options, array $additional_options = [])
-	{
-		return parent::render($target_or_options, $additional_options);
-	}
+
 }
